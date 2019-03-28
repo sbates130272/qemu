7484dd486531e30f158d55b9de3041410dc5113c
Martin Ichilevici de Oliveira <martin.i.oliveira@gmail.com>
2019-03-28 14:43:24 -0600

    Finish DMA implementation + commands support
    The DMA can now be used to load the ".data".
    
    Alternatively, the ".text" and ".data" can be used programmed by first
    copying to the p2p region and then issuing a command that moves the data
    from the p2p region to the appropriate regions in the device.
    
    There are three other commands as well in this patch:
    - Run program: start execution of the eBPF program
    - Dump memory: dumps the content of the program and data segments
    (currently, this only prints in the screen, it is not sent back to the
    guest).
    
    Signed-off-by: Martin Ichilevici de Oliveira <martin.i.oliveira@gmail.com>

--- hw/misc/pcie-ubpf.c
+++ hw/misc/pcie-ubpf.c
@@ -41,14 +41,23 @@
 #define DMA_START       0x40000
 #define DMA_SIZE        4096
 
-#define EBPF_PROG_LEN_OFFSET    0x0
+#define EBPF_TEXT_LEN_OFFSET    0x0
 #define EBPF_MEM_LEN_OFFSET     0x4
-#define EBPF_PROG_OFFSET        0x1000
+#define EBPF_TEXT_OFFSET        0x1000
 #define EBPF_RET_OFFSET         0x200000
 #define EBPF_READY_OFFSET       0x200004
 #define EBPF_REGS_OFFSET        0x200008
 #define EBPF_MEM_OFFSET         0x400000
+#define EBPF_P2P_OFFSET         0x800000
 #define EBPF_START              0x1
+
+#define EBPF_OFFLOAD_OPCODE_DMA_TEXT      0x00
+#define EBPF_OFFLOAD_OPCODE_MOVE_P2P_TEXT 0x01
+#define EBPF_OFFLOAD_OPCODE_DMA_DATA      0x02
+#define EBPF_OFFLOAD_OPCODE_MOVE_P2P_DATA 0x03
+#define EBPF_OFFLOAD_OPCODE_RUN_PROG      0x04
+#define EBPF_OFFLOAD_OPCODE_GET_REGS      0x05
+#define EBPF_OFFLOAD_OPCODE_DUMP_MEM      0xff
 
 #define EBPF_NOT_READY          0x0
 #define EBPF_READY              0x1
@@ -81,13 +90,13 @@ typedef struct {
 
     uint32_t irq_status;
 
-    struct dma_state {
+    struct command {
         uint8_t opcode;
         uint8_t ctrl;
         uint32_t length;
         uint32_t offset;
         uint64_t addr;
-    } dma;
+    } cmd;
     char dma_buf[DMA_SIZE];
     uint64_t dma_mask;
 } BpfState;
@@ -167,10 +176,10 @@ static void bpf_raise_irq(BpfState *bpf, uint32_t val)
 static int bpf_start_program(BpfState *bpf)
 {
     char *bpf_ram_ptr = (char*) memory_region_get_ram_ptr(&bpf->bpf_ram);
-    uint32_t code_len = *(uint32_t*) (bpf_ram_ptr + EBPF_PROG_LEN_OFFSET);
+    uint32_t code_len = *(uint32_t*) (bpf_ram_ptr + EBPF_TEXT_LEN_OFFSET);
     uint32_t mem_len =  *(uint32_t*) (bpf_ram_ptr + EBPF_MEM_LEN_OFFSET);
-    void *code = bpf_ram_ptr + EBPF_PROG_OFFSET;
-    void *mem  = bpf_ram_ptr + EBPF_MEM_OFFSET;
+    void *code = bpf_ram_ptr + EBPF_TEXT_OFFSET;
+    void *mem  = bpf_ram_ptr + EBPF_MEM_OFFSET + bpf->cmd.offset;
     uint64_t *regs = (uint64_t*) (bpf_ram_ptr + EBPF_REGS_OFFSET);
     bool *ready_addr = (bool*) (bpf_ram_ptr + EBPF_READY_OFFSET);
     uint64_t *ret_addr = (uint64_t*) (bpf_ram_ptr + EBPF_RET_OFFSET);
@@ -238,19 +247,19 @@ static uint64_t bpf_mmio_read(void *opaque, hwaddr addr, unsigned size)
 
     switch (addr) {
     case 0x0:
-        val = bpf->dma.opcode;
+        val = bpf->cmd.opcode;
         break;
     case 0x1:
-        val = bpf->dma.ctrl;
+        val = bpf->cmd.ctrl;
         break;
     case 0x4:
-        val = bpf->dma.length;
+        val = bpf->cmd.length;
         break;
     case 0x8:
-        val = bpf->dma.offset;
+        val = bpf->cmd.offset;
         break;
     case 0xc:
-        val = bpf->dma.addr;
+        val = bpf->cmd.addr;
         break;
     default:
         //fprintf(stderr, "Invalid read (reserved)\n");
@@ -260,61 +269,76 @@ static uint64_t bpf_mmio_read(void *opaque, hwaddr addr, unsigned size)
     return val;
 }
 
-static void program_text(BpfState *bpf)
+static void load_text(BpfState *bpf, bool inp2p)
 {
     char *bpf_ram_ptr = (char*) memory_region_get_ram_ptr(&bpf->bpf_ram);
-    void *code = bpf_ram_ptr + EBPF_PROG_OFFSET;
-    uint32_t *code_len = (uint32_t*) (bpf_ram_ptr + EBPF_PROG_LEN_OFFSET);
+    void *code = bpf_ram_ptr + EBPF_TEXT_OFFSET;
+    uint32_t *code_len = (uint32_t*) (bpf_ram_ptr + EBPF_TEXT_LEN_OFFSET);
 
-    pci_dma_read(&bpf->pdev, bpf->dma.addr, code + bpf->dma.offset, bpf->dma.length);
-    if (bpf->dma.offset == 0)
-        *code_len = bpf->dma.length;
+    if (inp2p)
+        memcpy(code, bpf_ram_ptr + EBPF_P2P_OFFSET + bpf->cmd.offset, bpf->cmd.length);
     else
-        *code_len += bpf->dma.length;
+        pci_dma_read(&bpf->pdev, bpf->cmd.addr, code + bpf->cmd.offset, bpf->cmd.length);
+    if (bpf->cmd.offset == 0)
+        *code_len = bpf->cmd.length;
+    else
+        *code_len += bpf->cmd.length;
 
-    bpf->dma.ctrl |= DMA_DONE;
+    atomic_or(&bpf->cmd.ctrl, DMA_DONE);
 }
 
-static void program_data(BpfState *bpf)
+static void load_data(BpfState *bpf, bool inp2p)
 {
+    char *bpf_ram_ptr = (char*) memory_region_get_ram_ptr(&bpf->bpf_ram);
+    void *mem = bpf_ram_ptr + EBPF_MEM_OFFSET;
+    uint32_t *mem_len =  (uint32_t*) (bpf_ram_ptr + EBPF_MEM_LEN_OFFSET);
+
+    if (inp2p)
+        memcpy(mem, bpf_ram_ptr + EBPF_P2P_OFFSET + bpf->cmd.offset, bpf->cmd.length);
+    else
+        pci_dma_read(&bpf->pdev, bpf->cmd.addr, mem + bpf->cmd.offset, bpf->cmd.length);
+    if (bpf->cmd.offset == 0)
+        *mem_len = bpf->cmd.length;
+    else
+        *mem_len += bpf->cmd.length;
+
+    atomic_or(&bpf->cmd.ctrl, DMA_DONE);
 }
 
 static void run_program(BpfState *bpf)
 {
-}
-
-static void get_registers(BpfState *bpf)
-{
+    bpf_stop_program(bpf);
+    bpf_start_program(bpf);
 }
 
 static void dump_memory(BpfState *bpf)
 {
     char *bpf_ram_ptr = (char*) memory_region_get_ram_ptr(&bpf->bpf_ram);
-    uint32_t code_len = *(uint32_t*) (bpf_ram_ptr + EBPF_PROG_LEN_OFFSET);
+    uint32_t code_len = *(uint32_t*) (bpf_ram_ptr + EBPF_TEXT_LEN_OFFSET);
     uint32_t mem_len =  *(uint32_t*) (bpf_ram_ptr + EBPF_MEM_LEN_OFFSET);
 
-    hexDump("prog", bpf_ram_ptr + EBPF_PROG_OFFSET, code_len);
+    hexDump("prog", bpf_ram_ptr + EBPF_TEXT_OFFSET, code_len);
     hexDump("data", bpf_ram_ptr + EBPF_MEM_OFFSET, mem_len);
 }
 
 static void process_command(BpfState *bpf)
 {
-    fprintf(stderr, "Process Command: Opcode: [0x%02x]\tLength: [%u]\tAddr: [0x%08lx]\n",
-            bpf->dma.opcode & 0xff, bpf->dma.length, bpf->dma.addr);
+    fprintf(stderr, "Process Command: Opcode: [0x%02x]\tLength: [%u]\tAddr: [0x%08lx]\tOffset: [0x%u]\n",
+            bpf->cmd.opcode, bpf->cmd.length, bpf->cmd.addr, bpf->cmd.offset);
 
-    switch (bpf->dma.opcode & 0xff) {
-        case 0x0:
-            program_text(bpf); break;
-        case 0x1:
-            program_data(bpf); break;
-        case 0x2:
+    switch (bpf->cmd.opcode) {
+        case EBPF_OFFLOAD_OPCODE_DMA_TEXT:
+        case EBPF_OFFLOAD_OPCODE_MOVE_P2P_TEXT:
+            load_text(bpf, bpf->cmd.opcode); break;
+        case EBPF_OFFLOAD_OPCODE_DMA_DATA:
+        case EBPF_OFFLOAD_OPCODE_MOVE_P2P_DATA:
+            load_data(bpf, bpf->cmd.opcode == EBPF_OFFLOAD_OPCODE_MOVE_P2P_DATA); break;
+        case EBPF_OFFLOAD_OPCODE_RUN_PROG:
             run_program(bpf); break;
-        case 0x3:
-            get_registers(bpf); break;
-        case 0xff:
+        case EBPF_OFFLOAD_OPCODE_DUMP_MEM:
             dump_memory(bpf); break;
         default:
-            fprintf(stderr, "Invalid opcode: %u\n", bpf->dma.opcode & 0xff);
+            fprintf(stderr, "Invalid opcode: %u\n", bpf->cmd.opcode & 0xff);
     }
 }
 
@@ -335,7 +359,7 @@ static void bpf_mmio_write(void *opaque, hwaddr addr, uint64_t val,
     switch (addr & 0xff) {
         case 0x0:
             check_size("opcode", 1, size);
-            bpf->dma.opcode = val & 0xff;
+            bpf->cmd.opcode = val & 0xff;
             break;
         case 0x1:
             check_size("ctrl", 1, size);
@@ -344,15 +368,15 @@ static void bpf_mmio_write(void *opaque, hwaddr addr, uint64_t val,
             break;
         case 0x4:
             check_size("length", 4, size);
-            bpf->dma.length = val & 0xffffffff;
+            bpf->cmd.length = val & 0xffffffff;
             break;
         case 0x8:
             check_size("offset", 4, size);
-            bpf->dma.offset = val & 0xffffffff;
+            bpf->cmd.offset = val & 0xffffffff;
             break;
         case 0xc:
             check_size("addr", 8, size);
-            bpf->dma.addr = val;
+            bpf->cmd.addr = val;
             break;
     }
 }
